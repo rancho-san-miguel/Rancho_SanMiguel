411c3def9b0c6572e1b677e81f208dc21b89f03a
dromark <lmcortes83@gmail.com>
2019-05-07 13:56:03 -0500

    Merge branch 'master' of D:\Usuarios\Lmcortes\Desktop\Escuela\Rancho_SanMiguel with conflicts.

--- Rancho_SanMiguel/apps/CRUD/models.py
+++ Rancho_SanMiguel/apps/CRUD/models.py
@@ -125,20 +125,21 @@ class HISTORIAL_VENTAS_BOVINO(models.Model):
 #     def __str__(self):
 #         return self.arete
 #
-# class CONTROL_GANADO(models.Model):
-#     arete = models.CharField(max_length=10)
-#     motivo = models.CharField(max_length=30)
-#     descripcion = models.TextField()
-#     lugar = models.CharField(max_length=100)
-#     fecha = models.DateTimeField()
-#     created = models.DateTimeField(auto_now_add=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     class Meta:
-#         ordering = ["id"]
-#
-#     def __str__(self):
-#         return self.arete
-#
+
+class CONTROL_GANADO(models.Model):
+    mot = Choices('Pesaje', 'Servicio')
+    arete = models.CharField(max_length=10)
+    motivo = models.CharField(choices=mot, max_length=15)
+    descripcion = models.TextField()
+    lugar = models.CharField(max_length=100)
+    fecha = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["id"]
+    def __str__(self):
+        return self.id
+
 # class HISTORIAL_VENTAS(models.Model):
 #     opciones = Choices('Ganado', 'Leche')
 #     tipo = models.CharField(choices=opciones)
